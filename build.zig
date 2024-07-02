e657762f78c09efc031fc2997bd9fdabf333b725
Jacob Stannix <jakestannix@gmail.com>
2024-07-02 17:17:09 -0600

    new blank slate

--- build.zig
+++ build.zig
@@ -2,11 +2,16 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
-
     const optimize = b.standardOptimizeOption(.{});
+
+    const opts = b.addOptions();
+    opts.addOption([:0]const u8, "title", "asteroids");
+    opts.addOption(i32, "default_width", 450);
+    opts.addOption(i32, "default_height", 800);
 
     const raylib_dep = b.dependency("raylib", .{});
     const raylib = raylib_dep.module("raylib");
+
     const exe = b.addExecutable(.{
         .name = "asteroids",
         .root_source_file = b.path("src/main.zig"),
@@ -14,6 +19,7 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
     exe.root_module.addImport("raylib", raylib);
+    exe.root_module.addOptions("opts", opts);
     b.installArtifact(exe);
 
     const run_cmd = b.addRunArtifact(exe);
--- src/main.zig
+++ src/main.zig
@@ -1,72 +1,23 @@
 const std = @import("std");
-const math = std.math;
+const opts = @import("opts");
 const ray = @import("raylib");
-const Vector2 = ray.Vector2;
-const Black = ray.colors.Black;
-const Color = ray.Color;
-const BASE_SIZE: f32 = 20;
-const screen = struct {
-    pub const width: i32 = 800;
-    pub const height: i32 = 450;
-    pub const fps: i32 = 60;
-};
-player: Player,
-const Game = @This();
+const colors = ray.colors;
+
 pub fn main() !void {
-    ray.initWindow(screen.width, screen.height, "asteroids");
-    var game = Game.init(.{
-        .player = .{
-            .pos = .{
-                (screen.width / 2) - (Player.WIDTH / 2),
-                (screen.height / 2) - (Player.HEIGHT / 2),
-            },
-            .rotation = 0,
-            .color = Black,
-        },
-    });
-    defer game.deinit();
+    ray.initWindow(opts.default_height, opts.default_width, opts.title);
+    defer ray.closeWindow();
+
     while (!ray.windowShouldClose()) {
         ray.beginDrawing();
         defer ray.endDrawing();
-        ray.clearBackground(ray.colors.White);
-        game.player.draw();
+        ray.clearBackground(colors.RayWhite);
+
+        ray.drawText(
+            opts.title,
+            (opts.default_height / 2) - (opts.title.len / 2) * 12,
+            (opts.default_width / 2) - 12,
+            24,
+            colors.Black,
+        );
     }
 }
-const InitOptions = struct {
-    player: Player,
-};
-fn init(options: InitOptions) Game {
-    ray.setTargetFps(screen.fps);
-    return Game{
-        .player = options.player,
-    };
-}
-fn deinit(self: *Game) void {
-    _ = self;
-    ray.closeWindow();
-}
-const Player = struct {
-    const x = 0;
-    const y = 1;
-    const Self = @This();
-    pub const HEIGHT: f32 = (BASE_SIZE / 2) / math.tan(math.degreesToRadians(20));
-    pub const WIDTH: f32 = BASE_SIZE;
-    pos: Vector2,
-    rotation: f32,
-    color: Color,
-    pub fn draw(self: *Player) void {
-        const p1 = Vector2{
-            self.pos[x] - (math.sin(math.degreesToRadians(self.rotation)) * HEIGHT),
-            self.pos[y] - (math.cos(math.degreesToRadians(self.rotation)) * HEIGHT),
-        };
-        const p2 = Vector2{
-            self.pos[x] - (math.cos(math.degreesToRadians(self.rotation)) * (WIDTH / 2)),
-            self.pos[y] + (math.sin(math.degreesToRadians(self.rotation)) * (WIDTH / 2)),
-        };
-        const p3 = Vector2{
-            self.pos[x] + (math.cos(math.degreesToRadians(self.rotation)) * (WIDTH / 2)),
-            self.pos[y] - (math.sin(math.degreesToRadians(self.rotation)) * (WIDTH / 2)),
-        };
-        ray.drawTriangle(p1, p2, p3, self.color);
-    }
-};
